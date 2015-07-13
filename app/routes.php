652cea9ae3bd2f331e24102a0c4fc6659ce44873
Michael Davies <mykudeez@gmail.com>
2015-07-13 17:55:45 +1200

    Added routes and user model

---
+++ app/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::get('/', CONTROLLERS.'home.php');
+
+Route::get('/register', CONTROLLERS.'register.php');
+Route::post('/register', CONTROLLERS.'register.php');
+
+Route::get('/logout', CONTROLLERS.'logout.php');
+
+Route::get('/login', CONTROLLERS.'login.php');
+Route::post('/login', CONTROLLERS.'login.php');
