7f9919a2630628ec439c07a4d88adaa17514f006
Michael Davies <michael.davies@44400-501.auckland.natcoll.ac.nz>
2015-07-23 17:05:55 +1200

    Updated WYSIWYG

--- app/views/footer.php
+++ app/views/footer.php
@@ -2,25 +2,9 @@
 	<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 	<script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
 	<script src="/assets/js/script.js"></script>
-	<script src="//tinymce.cachefly.net/4.2/tinymce.min.js"></script>
-	<script type="text/javascript">
-		tinymce.init({
-		    selector: "textarea",
-		    theme: "modern",
-		    plugins: [
-		        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-		        "searchreplace wordcount visualblocks visualchars code fullscreen",
-		        "insertdatetime media nonbreaking save table contextmenu directionality",
-		        "emoticons template paste textcolor colorpicker textpattern imagetools"
-		    ],
-		    toolbar1: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-		    toolbar2: "print preview media | forecolor backcolor emoticons",
-		    image_advtab: true,
-		    templates: [
-		        {title: 'Test template 1', content: 'Test 1'},
-		        {title: 'Test template 2', content: 'Test 2'}
-		    ]
-		});
-	</script>
+	<script src="http://cdn.ckeditor.com/4.5.1/basic/ckeditor.js"></script>
+	 <script>
+     	CKEDITOR.replace(textarea);
+     </script>
 </body>
 </html>
