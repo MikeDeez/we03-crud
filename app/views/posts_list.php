d66553e7a721ba73a89cd1b4d1d02eaa0fbc54fa
Michael Davies <mykudeez@gmail.com>
2015-07-23 16:03:15 +1200

    Fixing post with jumbotron

--- app/views/posts_list.php
+++ app/views/posts_list.php
@@ -2,13 +2,14 @@
 		<div class="posts flex flex-j-between">
 			<?foreach($posts->items as $post):?>
 				<div class="post jumbotron">
-					<div class="container"></div>
+					<div class="container">
 						<h2><?=$post->subject?></h2>
 						<p><?=$post->content?></p>
-				<?if(Auth::is_admin()):?>
-					<a href="<?=$post->id.'/edit'?>" class="btn btn-info">Edit</a>
-					<a href="<?=$post->id.'/delete'?>" class="btn btn-danger">Delete</a>
-				<?endif;?>
+						<?if(Auth::is_admin()):?>
+							<a href="<?=$post->id.'/edit'?>" class="btn btn-info">Edit</a>
+							<a href="<?=$post->id.'/delete'?>" class="btn btn-danger">Delete</a>
+						<?endif;?>
+					</div>
 				</div>
 				<hr>
 			<?endforeach;?>
